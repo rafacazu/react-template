6a37893dc2813e2b65688b609440e3a1c59e32d4
Rafael Cazuriaga <rafaelcazuriaga@Admins-MacBook-Pro.local>
2018-09-21 15:38:02 +1200

    adding test tools

---
+++ my-app/src/components/Item.test.js
@@ -0,0 +1,14 @@
+import expect from 'expect';
+import TestUtils from 'react-dom/test-utils';
+import Item from './Item';
+import React from 'react';
+import itemDetails from '../container/itemDetails';
+
+describe('somethig here', () => {
+  it('render something here', () => {
+    const result = 3;
+    const a = 2;
+    const b = 1
+    expect(result).toEqual(a+b);
+  });
+});
